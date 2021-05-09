ca382d88be584404c6bb394ff785475cabbfb3b1
DAC <dac@cogsmith.com>
2021-05-09 17:59:10 -0400

    keybindings

--- extension.js
+++ extension.js
@@ -1,43 +1,18 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-
-/**
- * @param {vscode.ExtensionContext} context
- */
-function activate(context) {
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "helloworld-minimal-sample" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.HELLOWORLD', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
+activate = function (context) {
+	let vscmd = vscode.commands.registerCommand('extension.HELLOWORLD', () => {
 		vscode.window.showInformationMessage('HELLOWORLD');
 	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(vscmd);
 
 	const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 10);
 	statusBarItem.command = 'extension.HELLOWORLD';
 	statusBarItem.text = 'HELLOWORLD';
 	statusBarItem.show();
 	context.subscriptions.push(statusBarItem);
-
 }
 
-// this method is called when your extension is deactivated
-function deactivate() { }
+deactivate = function () { }
 
-// eslint-disable-next-line no-undef
-module.exports = {
-	activate,
-	deactivate
-}
+module.exports = { activate, deactivate };

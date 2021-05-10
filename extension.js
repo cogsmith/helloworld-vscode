36261a47923354d6edbdbcaff49d75492618e1ea
DAC <dac@cogsmith.com>
2021-05-10 13:49:24 -0400

    TreeDataProvider

--- extension.js
+++ extension.js
@@ -5,7 +5,11 @@ App.Main = async function () {
 }
 
 activate = function (context) {
-	let vscmd = vscode.commands.registerCommand('extension.HELLOWORLD', async () => {
+	console.log('ACTIVATE-INIT');
+
+	//
+
+	const vscmd = vscode.commands.registerCommand('extension.HELLOWORLD', async () => {
 		vscode.window.showInformationMessage('INFO');
 		vscode.window.showWarningMessage('WARNING');
 		vscode.window.showErrorMessage('ERROR');
@@ -15,11 +19,44 @@ activate = function (context) {
 	});
 	context.subscriptions.push(vscmd);
 
+	//
+
 	const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 10);
 	statusBarItem.command = 'extension.HELLOWORLD';
 	statusBarItem.text = 'HELLOWORLD';
 	statusBarItem.show();
 	context.subscriptions.push(statusBarItem);
+
+	//
+
+	class myTreeDataProvider {
+		constructor() {
+			console.log('MyTreeDataProvider');
+			this.data = [];
+		}
+
+		getTreeItem(q) {
+			console.log({ GetTreeItem: q });
+			let treeitem = {
+				label: q,
+				tooltip: 'TOOLTIP',
+				collapsibleState: (q == 'LEAF') ? vscode.TreeItemCollapsibleState.None : vscode.TreeItemCollapsibleState.Collapsed
+			};
+			return treeitem;
+		}
+
+		getChildren(q) {
+			console.log({ GetChildren: q });
+			if (!q) { return ['CHILD1', 'CHILD2']; }
+			return ['NODE', 'LEAF'];
+		}
+	}
+
+	vscode.window.registerTreeDataProvider('HELLOWORLD_VIEW', new myTreeDataProvider());
+
+	//
+
+	console.log('ACTIVATE-DONE');
 }
 
 deactivate = function () { }
